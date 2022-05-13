5fa0d1326a86985ad5e92d177f8049583d5d978d
jiun <jiun48562@gmail.com>
2022-05-13 22:44:12 +0900

    [ADD] kakao map

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'OpenMap', apikey : process.env.KAKAO_JS_KEY });
 });
 
 module.exports = router;
