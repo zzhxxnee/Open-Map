6ff8cfab8d634605a6f11f5bf99ac71cb684eae7
JIHYUN LEE <20201008@sungshin.ac.kr>
2022-05-14 02:01:14 +0900

    업체등록페이지

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.get('/myPage/compRegist', (req,res,next) => {
+  res.render("compRegist");
+})
+
+router.get('/myPage/compRegist/popup/jusoPopup', (req, res)=>{
+  res.render('jusoPopup');
+});
+
+router.post('/myPage/compRegist/popup/jusoPopup', (req, res) => {
+  res.locals = req.body;
+  res.render('jusoPopup');
+});
+
 module.exports = router;
