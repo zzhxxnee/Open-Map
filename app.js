ea5d48d81f905acee7dd456f8d2f600eff424a8a
JIHYUN LEE <20201008@sungshin.ac.kr>
2022-06-09 17:40:59 +0900

    merge develop

--- app.js
+++ app.js
@@ -85,9 +85,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 module.exports = app;
 
